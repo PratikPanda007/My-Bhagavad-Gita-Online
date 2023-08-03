222a1f7de0fd4b094a382fc39adbce4061475e67
PratikPanda <pratic.panda@gmail.com>
2023-08-03 22:33:58 +0530

    Added Speech To Text Functionality from Microsoft

---
+++ BhagavadGita/BhagavadGita/Scripts/MicrosoftAudioDD.js
@@ -0,0 +1,29 @@
+﻿function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+        return;
+    }
+
+    const voices = speechSynthesis.getVoices();
+
+    for (let i = 0; i < voices.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+        if (voices[i].default) {
+            option.textContent += " — DEFAULT";
+        }
+
+        option.setAttribute("data-lang", voices[i].lang);
+        option.setAttribute("data-name", voices[i].name);
+        option.setAttribute("value", i);
+        document.getElementById("microsoftVoice").appendChild(option);
+    }
+}
+
+populateVoiceList();
+if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+}
